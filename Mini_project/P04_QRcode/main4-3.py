20ec2488f944b34c755c8d5470b67a0246a9951d
CallmeJuno <37017035+junhokim42@users.noreply.github.com>
2022-11-06 23:58:14 +0900

    master

---
+++ Mini_project/P04_QRcode/main4-3.py
@@ -0,0 +1,15 @@
+import qrcode
+
+file_path = r'P04_QRcode\QRcode_list.txt'
+with open(file_path, 'rt', encoding = 'UTF8') as f:
+    read_lines = f.readlines()
+
+    for line in read_lines:
+        line = line.strip()
+        print(line)
+
+        qr_data = line
+        qr_img = qrcode.make(qr_data)
+
+        save_path = 'P4_QRcode\\' + qr_data + '.png'
+        qr_img.save(save_path)
